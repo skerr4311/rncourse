1d011677d7236d7c7a2bebfe9cb69e693132f71b
SkerrHome <skerr@aucklanduni.ac.nz>
2018-12-10 14:52:04 +1300

    created more custom components to make app.js more tidy

--- App.js
+++ App.js
@@ -1,66 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
-import ListItem from './src/components/ListItem/ListItem';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 type Props = {};
 export default class App extends Component<Props> {
   state = {
-    placeName: '',
     places: []
   }
 
-  placeNameChangeHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === ""){
-      return;
-    }
+  placeAddedHandler = PlaceName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
+        places: prevState.places.concat(PlaceName)
       };
     });
   };
 
   render() {
-    const placesOutput = this.state.places.map((place, i) =>
-      <ListItem key={i} placeName={place} />
-    );
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput 
-          style={{width: 300}}
-          placeholder="Anawesome Place"
-          value={this.state.placeName} 
-          onChangeText={this.placeNameChangeHandler}
-          style={styles.placeInput}
-          />
-          <Button title="Add" style={styles.placeButton} 
-          onPress={this.placeSubmitHandler} 
-          />
-        </View> 
-        <View style={styles.listContainer}>{placesOutput}</View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
+        <PlaceList places={this.state.places} />
       </View>
     );
   }
@@ -83,21 +46,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-  },
-  inputContainer: {
-    // flex: 1, 
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  placeInput: {
-    width: "70%"
-  },
-  placeButton: {
-    width: "30%"
-  },
-  listContainer: {
-    width: "100%"
   }
 });
---
+++ src/components/PlaceInput/PlaceInput.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import {View, TextInput, Button, StyleSheet} from 'react-native';
+
+class PlaceInput extends Component {
+    state = {
+        placeName: ''
+    }
+    
+    placeNameChangeHandler = val => {
+        this.setState({
+            placeName: val
+        });
+    };
+
+    placeSubmitHandler = () => {
+        if (this.state.placeName.trim() === ""){
+          return;
+        }
+
+        this.props.onPlaceAdded(this.state.placeName)
+    };
+
+    render() {
+        return (
+            <View style={styles.inputContainer}>
+                <TextInput 
+                    style={{width: 300}}
+                    placeholder="Anawesome Place"
+                    value={this.state.placeName} 
+                    onChangeText={this.placeNameChangeHandler}
+                    style={styles.placeInput}
+                />
+                <Button title="Add" style={styles.placeButton} 
+                    onPress={this.placeSubmitHandler} 
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        // flex: 1, 
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+      },
+      placeInput: {
+        width: "70%"
+      },
+      placeButton: {
+        width: "30%"
+      }
+});
+
+export default PlaceInput;
