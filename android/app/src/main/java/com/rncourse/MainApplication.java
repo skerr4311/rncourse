9453320e98e1d33f98185110550d7ae65d49aa26
SkerrHome <skerr@aucklanduni.ac.nz>
2018-12-28 16:07:09 +1300

    Added react-native-navigation

--- android/app/src/main/java/com/rncourse/MainApplication.java
+++ android/app/src/main/java/com/rncourse/MainApplication.java
@@ -8,40 +8,73 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationApplication;
+//import com.reactnativenavigation.react.NavigationReactNativeHost;
+//import com.reactnativenavigation.react.ReactGateway;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+//  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//     @Override
+//     public boolean getUseDeveloperSupport() {
+//       return BuildConfig.DEBUG;
+//     }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new VectorIconsPackage()
-      );
-    }
+//     @Override
+//     protected List<ReactPackage> getPackages() {
+//       return Arrays.<ReactPackage>asList(
+//           new MainReactPackage(),
+//           new VectorIconsPackage()
+//       );
+//     }
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+//     @Override
+//     protected String getJSMainModuleName() {
+//       return "index";
+//     }
+//   };
+
+//   @Override
+//   public ReactNativeHost getReactNativeHost() {
+//     return mReactNativeHost;
+//   }
+
+//   @Override
+//   public void onCreate() {
+//     super.onCreate();
+//     SoLoader.init(this, /* native exopackage */ false);
+//   }
+// }
+  // @Override
+  // protected ReactGateway createReactGateway() {
+  //   ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+  //     @Override
+  //     protected String getJSMainModuleName(){
+  //       return "index";
+  //     }
+  //   };
+  //   return new ReactGateway(this, isDebug(), host);
+  // }
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    // make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+      new VectorIconsPackage()
+    );
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
   }
 }
