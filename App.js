fe66df40225152e9ae632a83392b467176c82e94
SkerrHome <skerr@aucklanduni.ac.nz>
2019-01-22 08:37:08 +1300

    Added a login page, beginning navigation

--- App.js
+++ App.js
@@ -1,84 +1,99 @@
 
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import { connect } from 'react-redux';
+// import React, {Component} from 'react';
+// import {StyleSheet, View} from 'react-native';
+// import { connect } from 'react-redux';
 
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceList from './src/components/PlaceList/PlaceList';
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions/index';
-//import placeImage from './src/assets/wolf.jpg'
+// import PlaceInput from './src/components/PlaceInput/PlaceInput';
+// import PlaceList from './src/components/PlaceList/PlaceList';
+// import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+// import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions/index';
+// //import placeImage from './src/assets/wolf.jpg'
 
-class App extends Component {
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName);
-    console.log('Place Added!');
-  };
+// class App extends Component {
+//   placeAddedHandler = placeName => {
+//     this.props.onAddPlace(placeName);
+//     console.log('Place Added!');
+//   };
 
-  placeDeletedHandler = () => {
-    this.props.onDeletePlace();
-  };
+//   placeDeletedHandler = () => {
+//     this.props.onDeletePlace();
+//   };
 
-  modalClosedHandler = () => {
-    this.props.onDeselectPlace();
-  };
+//   modalClosedHandler = () => {
+//     this.props.onDeselectPlace();
+//   };
 
-  placeSelectedHandler = key => {
-    this.props.onSelectPlace(key);
-  };
+//   placeSelectedHandler = key => {
+//     this.props.onSelectPlace(key);
+//   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail 
-        selectedPlace={this.props.selectedPlace}
-        onItemDeleted={this.placeDeletedHandler}
-        onModalClosed={this.modalClosedHandler} 
-        />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-        <PlaceList 
-          places={this.props.places} 
-          onItemSelected={this.placeSelectedHandler}
-        />
-      </View>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <PlaceDetail 
+//         selectedPlace={this.props.selectedPlace}
+//         onItemDeleted={this.placeDeletedHandler}
+//         onModalClosed={this.modalClosedHandler} 
+//         />
+//         <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
+//         <PlaceList 
+//           places={this.props.places} 
+//           onItemSelected={this.placeSelectedHandler}
+//         />
+//       </View>
+//     );
+//   }
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 26,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 26,
+//     justifyContent: 'flex-start',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   }
+// });
+
+// const mapStateToProps = state => {
+//   return {
+//     places: state.places.places,
+//     selectedPlace: state.places.selectedPlace
+//   };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     onAddPlace: (name) => dispatch(addPlace(name)),
+//     onDeletePlace: () => dispatch(deletePlace()),
+//     onSelectPlace: (key) => dispatch(selectPlace(key)),
+//     onDeselectPlace: () => dispatch(deselectPlace())
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+import { Navigation } from 'react-native-navigation';
+
+import AuthScreen from './src/screens/Auth/Auth';
+
+//Register Screens
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+
+//Start App
+Navigation.startSingleScreenApp({
+  screen:{
+    screen: "awesome-places.AuthScreen",
+    title: "Login"
   }
 });
-
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: (name) => dispatch(addPlace(name)),
-    onDeletePlace: () => dispatch(deletePlace()),
-    onSelectPlace: (key) => dispatch(selectPlace(key)),
-    onDeselectPlace: () => dispatch(deselectPlace())
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
