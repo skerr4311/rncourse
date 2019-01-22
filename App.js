baeeb38c806b276398eca54962b5449f79622ea4
SkerrHome <skerr@aucklanduni.ac.nz>
2019-01-22 22:16:54 +1300

    Added options icon and implemented a side drawer menu

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import configureStore from './src/store/configureStore';
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 
 const store = configureStore();
 
@@ -14,6 +15,7 @@ Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen, stor
 Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
 Navigation.registerComponent("awesome-places.PlaceDetailScreen", () => PlaceDetailScreen, store, Provider);
+Navigation.registerComponent("awesome-places.SideDrawer", () => SideDrawer)
 
 //Start App
 Navigation.startSingleScreenApp({
