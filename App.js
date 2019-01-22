2d1f88535f2d5178ed7a702f7993a42cfd7cad72
SkerrHome <skerr@aucklanduni.ac.nz>
2019-01-22 20:22:13 +1300

    Added the delete function

--- App.js
+++ App.js
@@ -1,88 +1,3 @@
-
-// import React, {Component} from 'react';
-// import {StyleSheet, View} from 'react-native';
-// import { connect } from 'react-redux';
-
-// import PlaceInput from './src/components/PlaceInput/PlaceInput';
-// import PlaceList from './src/components/PlaceList/PlaceList';
-// import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-// import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions/index';
-// //import placeImage from './src/assets/wolf.jpg'
-
-// class App extends Component {
-//   placeAddedHandler = placeName => {
-//     this.props.onAddPlace(placeName);
-//     console.log('Place Added!');
-//   };
-
-//   placeDeletedHandler = () => {
-//     this.props.onDeletePlace();
-//   };
-
-//   modalClosedHandler = () => {
-//     this.props.onDeselectPlace();
-//   };
-
-//   placeSelectedHandler = key => {
-//     this.props.onSelectPlace(key);
-//   };
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <PlaceDetail 
-//         selectedPlace={this.props.selectedPlace}
-//         onItemDeleted={this.placeDeletedHandler}
-//         onModalClosed={this.modalClosedHandler} 
-//         />
-//         <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-//         <PlaceList 
-//           places={this.props.places} 
-//           onItemSelected={this.placeSelectedHandler}
-//         />
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 26,
-//     justifyContent: 'flex-start',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   }
-// });
-
-// const mapStateToProps = state => {
-//   return {
-//     places: state.places.places,
-//     selectedPlace: state.places.selectedPlace
-//   };
-// };
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     onAddPlace: (name) => dispatch(addPlace(name)),
-//     onDeletePlace: () => dispatch(deletePlace()),
-//     onSelectPlace: (key) => dispatch(selectPlace(key)),
-//     onDeselectPlace: () => dispatch(deselectPlace())
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
-
 import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
 
@@ -98,7 +13,7 @@ const store = configureStore();
 Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen, store, Provider);
 Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
-Navigation.registerComponent("awesome-places.PlaceDetailScreen", () => PlaceDetailScreen);
+Navigation.registerComponent("awesome-places.PlaceDetailScreen", () => PlaceDetailScreen, store, Provider);
 
 //Start App
 Navigation.startSingleScreenApp({
--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Image, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import { deletePlace } from '../../store/actions/index';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const placeDetail = props => {
-    return (
+class PlaceDetail extends Component {
+    placeDeletedHandler = () => {
+        this.props.onDeletePlace(this.props.selectedPlace.key);
+        this.props.navigator.pop();
+    }
+
+    render () {
+        return (
             <View style={styles.container}>
                 <View>
-                    <Image source={props.selectedPlace.image} style={styles.placeImage} />
-                    <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+                    <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
+                    <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
                 </View>
                 <View>
-                    <TouchableOpacity onPress={props.onItemDeleted}>
+                    <TouchableOpacity onPress={this.placeDeletedHandler}>
                         <View style={styles.deleteButton}>
                             <Icon size={30} name="ios-trash" color="red" />
                         </View>
                     </TouchableOpacity>
                 </View>
             </View>
-    )
-};
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -39,4 +48,10 @@ const styles = StyleSheet.create({
     }
 });
 
-export default placeDetail;
+const mapDispatchToProps = dispatch => {
+    return {
+        onDeletePlace: (key) => dispatch(deletePlace(key))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(PlaceDetail);
