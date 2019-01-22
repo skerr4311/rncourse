c875ded65b95891318a76cc5910d96dd73d587d4
SkerrHome <skerr@aucklanduni.ac.nz>
2019-01-22 16:30:39 +1300

    made the items of the list in 'Find Place' clickable

--- App.js
+++ App.js
@@ -84,15 +84,21 @@
 // export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import configureStore from './src/store/configureStore';
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+
+const store = configureStore();
 
 //Register Screens
-Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
-Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen)
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen, store, Provider);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
+Navigation.registerComponent("awesome-places.PlaceDetailScreen", () => PlaceDetailScreen);
 
 //Start App
 Navigation.startSingleScreenApp({
--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE} from './actionTypes';
+import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName) => {
     return {
@@ -13,15 +13,3 @@ export const deletePlace = () => {
     };
 };
 
-export const selectPlace = (key) => {
-    return {
-        type: SELECT_PLACE,
-        placeKey: key
-    };
-};
-
-export const deselectPlace = () => {
-    return {
-        type: DESELECT_PLACE
-    }
-}
