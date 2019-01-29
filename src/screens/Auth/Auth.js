f693d978de1eb7793777ff364754b7116c55e857
SkerrHome <skerr@aucklanduni.ac.nz>
2019-01-30 11:46:00 +1300

    Added custom button

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -6,6 +6,7 @@ import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 import HeadingText from "../../components/UI/HeadingText/HeadingText";
 import MainText from "../../components/UI/MainText/MainText";
 import backgroundImage from "../../assets/background.jpg";
+import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
 
 class AuthScreen extends Component {
     loginHandler = () => {
@@ -19,13 +20,15 @@ class AuthScreen extends Component {
                     <MainText>
                         <HeadingText>Please Log In</HeadingText>
                     </MainText>
-                    <Button title="Switch to Login" />
+                    <ButtonWithBackground color="#29aaf4" onPress={() => alert("Hello")}>Switch to Login</ButtonWithBackground>
                     <View style={styles.inputContainer}>
                         <DefaultInput placeholder="Your E-mail Address" style={styles.input}/>
                         <DefaultInput placeholder="Password" style={styles.input}/>
                         <DefaultInput placeholder="Confirm Password" style={styles.input}/>
                     </View>
-                    <Button title="Submit" onPress={this.loginHandler} />
+                    <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>
+                        Submit
+                    </ButtonWithBackground>
                 </View>
             </ImageBackground>
         );
