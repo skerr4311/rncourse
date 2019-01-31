60b61cc986a09d4f3a75dd9d3fa36c6e175cf070
SkerrHome <skerr@aucklanduni.ac.nz>
2019-02-01 11:37:50 +1300

    re hooked up the share button in 'Share Place'

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -3,27 +3,14 @@ import {View, TextInput, Button, StyleSheet} from 'react-native';
 
 import DefaultInput from "../UI/DefaultInput/DefaultInput";
 
-class PlaceInput extends Component {
-    state = {
-        placeName: ''
-    }
+const placeInput = props => (
+    <DefaultInput 
+        placeholder="Place Name" 
+        value={props.placeName}
+        onChangeText={props.onChangeText}
+    />
+);
     
-    placeNameChangeHandler = val => {
-        this.setState({
-            placeName: val
-        });
-    };
 
 
-    render() {
-        return (
-            <DefaultInput 
-                placeholder="Place Name" 
-                value={this.state.placeName}
-                onChangeText={this.placeNameChangeHandler}
-            />
-        );
-    }
-}
-
-export default PlaceInput;
+export default placeInput;
