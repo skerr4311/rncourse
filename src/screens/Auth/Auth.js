2820c5d5671807fd72d0b60554169cd0f87e90ee
SkerrHome <skerr@aucklanduni.ac.nz>
2019-02-05 13:35:56 +1300

    made the app adjust content when rotated

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
+import { 
+    View, 
+    Text, 
+    Button, 
+    TextInput, 
+    StyleSheet, 
+    ImageBackground,
+    Dimensions 
+} from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
@@ -9,22 +17,64 @@ import backgroundImage from "../../assets/background.jpg";
 import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
 
 class AuthScreen extends Component {
+    state = {
+        respStyles: {
+            pwContainerDirection: "column",
+            pwContainerJustifyContent: "flex-start",
+            pwWrapperWidth: "100%"
+        }
+    };
+    constructor(props) {
+        super(props);
+        Dimensions.addEventListener("change", (dims) => {
+            this.setState({
+                respStyles: {
+                    pwContainerDirection: Dimensions.get("window"). height > 500 ? "column" : "row",
+                    pwContainerJustifyContent: Dimensions.get("window"). height > 500 ? "flex-start" : "space-between",
+                    pwWrapperWidth: Dimensions.get("window"). height > 500 ? "100%" : "45%"
+                }
+            });
+        });
+    }
+
     loginHandler = () => {
         startMainTabs();
     }
 
     render () {
+        let headingText = null;
+
+        if (Dimensions.get('window').height > 500) {
+            headingText = (
+                <MainText>
+                    <HeadingText>Please Log In</HeadingText>
+                </MainText>
+            );
+        }
+
         return (
             <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
                 <View style={styles.container}>
-                    <MainText>
-                        <HeadingText>Please Log In</HeadingText>
-                    </MainText>
+                    {headingText}
                     <ButtonWithBackground color="#29aaf4" onPress={() => alert("Hello")}>Switch to Login</ButtonWithBackground>
                     <View style={styles.inputContainer}>
                         <DefaultInput placeholder="Your E-mail Address" style={styles.input}/>
-                        <DefaultInput placeholder="Password" style={styles.input}/>
-                        <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+                        <View style={{
+                                    flexDirection: this.state.respStyles.pwContainerDirection,
+                                    justifyContent: this.state.respStyles.pwContainerJustifyContent
+                                }}>
+                            <View 
+                                style={{
+                                    width: this.state.respStyles.pwWrapperWidth
+                                }}>
+                                <DefaultInput placeholder="Password" style={styles.input}/>
+                            </View>
+                            <View style={{
+                                    width: this.state.respStyles.pwWrapperWidth
+                                }}>
+                                <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+                            </View>
+                        </View>
                     </View>
                     <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>
                         Submit
@@ -51,6 +101,13 @@ const styles = StyleSheet.create({
     backgroundImage: {
         width: "100%",
         flex: 1
+    },
+    passwordContainer: {
+        flexDirection: Dimensions.get("window"). height > 500 ? "column" : "row",
+        justifyContent: "space-between"
+    },
+    passwordWrapper: {
+        width: Dimensions.get("window"). height > 500 ? "100%" : "45%"
     }
 });
 
