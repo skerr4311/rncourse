64408708b0596a3ca41ee22e9bd6918770f6b8cf
SkerrHome <skerr@aucklanduni.ac.nz>
2019-02-09 09:59:45 +1300

    added animation to the list of places

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -11,7 +11,8 @@ class FindPlaceScreen extends Component {
 
     state = {
         placesLoaded: false,
-        removeAnim: new Animated.Value(1)
+        removeAnim: new Animated.Value(1),
+        placesAnim: new Animated.Value(0)
     }
     constructor(props) {
         super(props);
@@ -28,12 +29,25 @@ class FindPlaceScreen extends Component {
         }
     }
 
+    placesLoadedHandler = () => {
+        Animated.timing(this.state.placesAnim, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true
+        }).start();
+    }
+
     placesSearchHandler = () => {
         Animated.timing(this.state.removeAnim, {
             toValue: 0,
             duration: 500,
             useNativeDriver: true
-        }).start();
+        }).start(() => {
+            this.setState({
+                placesLoaded: true
+            });
+            this.placesLoadedHandler();
+        });
     };
     
     itemSelectedHandler = key => {
@@ -70,10 +84,15 @@ class FindPlaceScreen extends Component {
         );
         if (this.state.placesLoaded) {
             content = (
-                <PlaceList 
-                places={this.props.places} 
-                onItemSelected={this.itemSelectedHandler}
-                />
+                <Animated.View 
+                    style={{
+                        opacity: this.state.placesAnim
+                    }}>
+                    <PlaceList 
+                        places={this.props.places} 
+                        onItemSelected={this.itemSelectedHandler}
+                    />
+                </Animated.View>
             );
         }
         return (
