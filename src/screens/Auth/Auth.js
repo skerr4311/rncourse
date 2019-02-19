0dac3cea0161d91b8cd1c3c8b09bc87ce5d4a2d7
SkerrHome <skerr@aucklanduni.ac.nz>
2019-02-19 22:15:43 +1300

    started setting up input test for auth screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -18,7 +18,30 @@ import ButtonWithBackground from "../../components/UI/ButtonWithBackground/Butto
 
 class AuthScreen extends Component {
     state = {
-        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape",
+        controls: {
+            email: {
+                value: "",
+                valid: false,
+                validationRules:{
+                    isEmail: true
+                }
+            },
+            password: {
+                value: "",
+                valid: false,
+                validationRules:{
+                    minLength: 6
+                }
+            },
+            confirmPassword: {
+                value: "",
+                valid: false,
+                validationRules:{
+                    equalTo: "password"
+                }
+            }
+        }
     };
     constructor(props) {
         super(props);
@@ -37,7 +60,21 @@ class AuthScreen extends Component {
 
     loginHandler = () => {
         startMainTabs();
-    }
+    };
+
+    updateInputState = (key, value) => {
+        this.setState(prevState => {
+            return {
+                controls: {
+                    ...prevState.controls,
+                    [key]: {
+                        ...prevState.controls[key],
+                        value: value
+                    }
+                }
+            };
+        });
+    };
 
     render () {
         let headingText = null;
@@ -56,7 +93,12 @@ class AuthScreen extends Component {
                     {headingText}
                     <ButtonWithBackground color="#29aaf4" onPress={() => alert("Hello")}>Switch to Login</ButtonWithBackground>
                     <View style={styles.inputContainer}>
-                        <DefaultInput placeholder="Your E-mail Address" style={styles.input}/>
+                        <DefaultInput 
+                            placeholder="Your E-mail Address" 
+                            style={styles.input}
+                            value={this.state.controls.email.value}
+                            onChangeText={(val) => this.updateInputState('email', val)}
+                        />
                         <View style={
                             this.state.viewMode === "portrait"
                             ? styles.portraitPasswordContainer
@@ -68,14 +110,24 @@ class AuthScreen extends Component {
                                     ? styles.portraitPasswordWrapper
                                     : styles.landscapePasswordWrapper
                                 }>
-                                <DefaultInput placeholder="Password" style={styles.input}/>
+                                <DefaultInput 
+                                    placeholder="Password" 
+                                    style={styles.input}
+                                    value={this.state.controls.password.value}
+                                    onChangeText={(val) => this.updateInputState("password", val)}
+                                />
                             </View>
                             <View style={
                                     this.state.viewMode === "portrait"
                                     ? styles.portraitPasswordWrapper
                                     : styles.landscapePasswordWrapper
                                 }>
-                                <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+                                <DefaultInput 
+                                    placeholder="Confirm Password" 
+                                    style={styles.input}
+                                    value={this.state.controls.confirmPassword.value}
+                                    onChangeText={(val) => this.updateInputState("confirmPassword", val)}
+                                />
                             </View>
                         </View>
                     </View>
