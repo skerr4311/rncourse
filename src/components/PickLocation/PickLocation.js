9f84e19761aee0dea074392af41aedd843a1e1d0
SkerrHome <sker078@aucklanduni.ac.nz>
2019-06-18 12:20:36 +1200

    added some functionallity to the find me button

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -35,6 +35,24 @@ class PickLocation extends Component {
         });
     }
 
+    getLocationHandler = () => {
+        navigator.geolocation.getCurrentPosition(pos => {
+            const coordsEvent = {
+                nativeEvent: {
+                    coordinate: {
+                        latitude: pos.coords.latitude,
+                        longitude: pos.coords.longitude
+                    }
+                }
+            };
+            this.pickLocationHandler(coordsEvent);
+        },
+    err => {
+        console.log(err);
+        alert("Fetching the position failed, please pick one manually!");
+    })
+    }
+
     render() {
         let marker = null;
 
@@ -52,7 +70,7 @@ class PickLocation extends Component {
                     {marker}
                 </MapView>  
                 <View style={styles.button}>
-                    <Button title="Locate Me" onPress={() => alert('PickImage')} />
+                    <Button title="Locate Me" onPress={this.getLocationHandler} />
                 </View>
             </View>
         );
